37a6c45c82f5c1b5c8d9c1dcd5b71d9eb9b1fa2e
nabe-kurage <tabiflag@gmail.com>
2020-10-17 22:03:37 +0900

    サバイバルTS読み進め中

--- src/increment.js
+++ src/increment.js
@@ -7,3 +7,46 @@ console.log(increment(1));
 // node src/increment.jsで確認
 var str = 90;
 console.log(str.length);
+// undefined
+console.log(typeof null);
+//これはオブジェクトになるのね
+var thisisnull = null;
+console.log(thisisnull === null);
+//これでnullは判定可能（true）
+// リテラル型
+// 決まった値を入れるもの
+var person = 1;
+person = 2;
+// person = 6; はエラーになる
+// 以下はすべて同じ
+function increment2(num) {
+    return num + 1;
+}
+var increment3 = function (num) {
+    return num + 1;
+};
+var increment4 = function (num) {
+    return num + 1;
+};
+var increment5 = function (num) { return num + 1; };
+// ？つけると省略できる
+function distance(p1, p2) {
+    var p0 = p2;
+    if (p0 === undefined) {
+        p0 = {
+            x: 0,
+            y: 0
+        };
+    }
+    return Math.pow((Math.pow((p1.x - p0.x), 2) + Math.pow((p1.y - p0.y), 2)), (1 / 2));
+}
+function inverse(p) {
+    return {
+        x: p.y,
+        y: p.x
+    };
+}
+function distance2(p1, p2) {
+    if (p2 === void 0) { p2 = inverse(p1); }
+    return Math.pow((Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2)), (1 / 2));
+}
--- src/increment.ts
+++ src/increment.ts
@@ -10,3 +10,61 @@ console.log(increment(1));
 const str: any = 90;
 console.log(str.length);
 // undefined
+
+console.log(typeof null);
+//これはオブジェクトになるのね
+const thisisnull = null;
+console.log(thisisnull === null)
+//これでnullは判定可能（true）
+
+// リテラル型
+// 決まった値を入れるもの
+let person: 1 | 2 | 3 | 4 = 1;
+person = 2;
+// person = 6; はエラーになる
+
+// 以下はすべて同じ
+function increment2(num: number): number {
+    return num + 1;
+}
+
+const increment3 = function(num: number): number {
+    return num + 1;
+}
+
+const increment4 = (num: number): number => {
+    return num + 1;
+}
+
+const increment5 = (num: number): number => num + 1;
+
+
+// typeの定義をできる
+type Point = {
+    x: number;
+    y: number
+}
+// ？つけると省略できる
+function distance(p1: Point, p2?: Point): number {
+    let p0: Point | undefined = p2;
+
+    if (p0 === undefined) {
+        p0 = {
+            x: 0,
+            y: 0
+        };
+    }
+
+    return ((p1.x - p0.x) ** 2 + (p1.y - p0.y) ** 2) ** (1 / 2);
+}
+
+function inverse(p: Point): Point {
+    return {
+        x: p.y,
+        y: p.x
+    };
+}
+
+function distance2(p1: Point, p2: Point = inverse(p1)): number {
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** (1 / 2);
+}
